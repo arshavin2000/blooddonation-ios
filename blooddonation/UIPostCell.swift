c351fa365bf84c36f9af70d52c1b7a4cbc3aa8c7
HAFFEZ <haffez23@gmail.com>
2019-04-25 22:21:44 +0100

    Create Home Screen
    Table View and Model cell

---
+++ blooddonation/UIPostCell.swift
@@ -0,0 +1,28 @@
+//
+//  UIPostCell.swift
+//  blooddonation
+//
+//  Created by user on 4/25/19.
+//  Copyright © 2019 Esprit. All rights reserved.
+//
+
+import UIKit
+
+class UIPostCell: UITableViewCell {
+    @IBOutlet weak var laProfilImg: UIImageView!
+    @IBOutlet weak var laUsername: UILabel!
+    
+    @IBOutlet weak var laDate: UILabel!
+    @IBOutlet weak var laPostImg: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
